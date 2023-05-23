40d6940fbfaf3e2a0ac4993795e7191d832c54c9
User <you@example.com>
2023-05-24 00:07:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      todoList: [],
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleInputChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleCreateTodo = () => {
+    const { inputValue, todoList } = this.state;
+    if (inputValue.trim() === "") {
+      return;
+    }
+  
+    const update = todoList.concat(inputValue);
+  
+    this.setState({
+      inputValue: "",
+      todoList: update,
+    });
+  };
+  
+
+  render() {
+    const { inputValue, todoList } = this.state;
+
+    return (
+      <div><div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={this.handleInputChange}
+          placeholder="Введите задачу"
+        />
+        <button onClick={this.handleCreateTodo}>
+          Сохранить
+        </button>
+        </div>
+        <div>
+          {todoList.map((todo, index) => (
+            <div key={index}>
+              <h3>{todo}</h3>
+            </div>
+          ))}
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
